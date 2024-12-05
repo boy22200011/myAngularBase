92ae9e2febc24a7fd75b34d23dbd4a7676b6163f
nickhung <nick@microsys.com.tw>
2024-12-05 16:35:19 +0800

    feat:
    1. init layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './modules/login/login.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: 'dashboard', component: DashboardComponent }],
+  },
+  { path: '**', redirectTo: 'login' },
+];
