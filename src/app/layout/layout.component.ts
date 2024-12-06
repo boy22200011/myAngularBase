909e90a6a2ed3ece7c5876fafd2e4b6d6fc6728d
nickhung <nick@microsys.com.tw>
2024-12-06 09:27:59 +0800

    feat: test

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatGridListModule } from '@angular/material/grid-list';
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatGridListModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  food = 'asdasdasdasd';
+
+  async addContact(){
+    
+  }
+}
